f9432864d59e305be22c0baf559de8d12f699871
huallison <116513851+huallison@users.noreply.github.com>
2022-12-11 12:47:57 -0300

    Add files via upload

---
+++ estilos/botton.js
@@ -0,0 +1,8 @@
+function enviar(){
+    var valor = document.getElementById("textinput").value;
+    if(valor === "itachi-22/11/2022"){
+        window.location.href='html/fala.html';
+    }else{
+        alert("SENHA INCORRETA !!!")
+    }
+}
